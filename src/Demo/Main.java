c64f6be1f787b5551b53fa69f211dc8562e8227b
lsj <lsj9383@163.com>
2016-11-13 17:26:31 +0800

    add baidudispatch

--- src/Demo/Main.java
+++ src/Demo/Main.java
@@ -1,14 +1,14 @@
 package Demo;
 
+import com.lsj.Trans.BaiduDispatch;
 import com.lsj.Trans.YouDaoDispatch;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-		YouDaoDispatch dispatch = new YouDaoDispatch();
 		
-		System.out.println(dispatch.Trans(null, null, "i am king"));
-		System.out.println(dispatch.Trans(null, null, "i am king"));
+		System.out.println(new YouDaoDispatch().Trans(null, null, "i am king"));
+		System.out.println(new BaiduDispatch().Trans(null, null, "i am king"));
 	}
 
 }
---
+++ src/com/lsj/Trans/BaiduDispatch.java
@@ -0,0 +1,20 @@
+package com.lsj.Trans;
+
+public class BaiduDispatch extends Dispatch {
+	public BaiduDispatch(){
+		this.base = "http://fanyi.baidu.com/v2transapi";
+	}
+	
+	public String Trans(String from, String targ, String query) throws Exception{
+		
+		HttpForm form = new HttpForm();
+		
+		form.put("from", "en");
+		form.put("to", "zh");
+		form.put("query", query);
+		form.put("transtype", "translang");
+		form.put("simple_means_flag", "3");
+		
+		return execute(form);
+	}
+}
