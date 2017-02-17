8cd79cc8e61aa2576f23f0af464fed370884443f
lsj <lsj9383@163.com>
2017-02-17 22:43:18 +0800

    fix bug about url path wrong when miss space

--- src/com/lsj/trans/factory/AbstractTranslatorFactory.java
+++ src/com/lsj/trans/factory/AbstractTranslatorFactory.java
@@ -1,6 +1,8 @@
 package com.lsj.trans.factory;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,20 +18,20 @@ public abstract class AbstractTranslatorFactory implements TFactory {
 	protected Map<String, Translator> translatorMap = new HashMap<>();
 	private List<String> workPackages = new ArrayList<>();
 	
-	public AbstractTranslatorFactory() throws ClassNotFoundException, InstantiationException, IllegalAccessException, DupIdException{
+	public AbstractTranslatorFactory() throws ClassNotFoundException, InstantiationException, IllegalAccessException, DupIdException, URISyntaxException{
 		workPackages.add("com.lsj.trans.impl");
 		initTranslator();
 	}
 	
-	public AbstractTranslatorFactory(String[] workPackages) throws ClassNotFoundException, InstantiationException, IllegalAccessException, DupIdException{
+	public AbstractTranslatorFactory(String[] workPackages) throws ClassNotFoundException, InstantiationException, IllegalAccessException, DupIdException, URISyntaxException{
 		this();
 	}
 	
-	public AbstractTranslatorFactory(String workPackage) throws ClassNotFoundException, InstantiationException, IllegalAccessException, DupIdException{
+	public AbstractTranslatorFactory(String workPackage) throws ClassNotFoundException, InstantiationException, IllegalAccessException, DupIdException, URISyntaxException{
 		this(new String[]{workPackage});
 	}
 	
-	private void initTranslator() throws ClassNotFoundException, InstantiationException, IllegalAccessException, DupIdException{
+	private void initTranslator() throws ClassNotFoundException, InstantiationException, IllegalAccessException, DupIdException, URISyntaxException{
 		for(String workPackage : workPackages){
 			List<String> workClassesName = getClassNameByPackage(workPackage);
 			for(String workClassName : workClassesName){
@@ -47,11 +49,11 @@ public abstract class AbstractTranslatorFactory implements TFactory {
 		}
 	}
 	
-	private List<String> getClassNameByPackage(String packageName){
+	private List<String> getClassNameByPackage(String packageName) throws URISyntaxException{
 		List<String> classesName = new ArrayList<>();
 		ClassLoader loader = getClass().getClassLoader();
 		URL url = loader.getResource(packageName.replace(".", "/"));
-		File packageDir = new File(url.getPath());
+		File packageDir = new File(new URI(url.getPath()).getPath());
 		for(File classFile : packageDir.listFiles()){
 			String classNickName = classFile.getName();
 			classNickName = classNickName.substring(0, classNickName.indexOf('.'));
