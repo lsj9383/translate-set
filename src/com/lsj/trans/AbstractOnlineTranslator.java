fba2d47a205acf4420642af75abe03aced81a182
lsj <lsj9383@163.com>
2017-02-20 14:03:35 +0800

    delete redundancy

--- src/com/lsj/trans/AbstractOnlineTranslator.java
+++ src/com/lsj/trans/AbstractOnlineTranslator.java
@@ -4,17 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class AbstractOnlineTranslator implements Translator {
-	protected static Map<String, AbstractOnlineTranslator> classMap = new HashMap<>();			//类名映射，由子类完成
 	protected Map<LANG, String> langMap = new HashMap<>();					//语言映射，由子类完成
 	
 	@Override
 	final public String trans(LANG from, LANG targ, String query) throws Exception {
 		String result = parseString(getResponse(from, targ, query));
 		return result;
-	}
-	
-	static public AbstractOnlineTranslator getInstance(String name) throws Exception{
-		return classMap.get(name);
 	}
 	
 	abstract protected String getResponse(LANG from, LANG targ, String query) throws Exception ;
