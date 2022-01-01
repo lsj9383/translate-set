0092fc2eae0c8172a38586155d6bdf168b38b8a0
Suat ORUC <93098605+suatoruc@users.noreply.github.com>
2022-01-01 15:47:32 +0300

    Update TransTest.java

--- src/test/TransTest.java
+++ src/test/TransTest.java
@@ -18,22 +18,22 @@ public class TransTest {
 	@Test
 	public void test() throws Exception {
 		String origin = "Xamarin.Forms has several layouts and features for organizing content on screen.\n Each layout control is described below, as well as details on how to handle screen orientation changes:";
-		System.out.println("金山 : " + factory.get("jinshan").trans(LANG.EN, LANG.ZH, origin));
-		System.out.println("有道 : " + factory.get("youdao").trans(LANG.EN, LANG.ZH, origin));
-		System.out.println("百度 : " + factory.get("baidu").trans(LANG.EN, LANG.ZH, origin));
-		System.out.println("谷歌 : " + factory.get("google").trans(LANG.EN, LANG.ZH, origin));
-		System.out.println("腾讯 : " + factory.get("tencent").trans(LANG.EN, LANG.ZH, origin));
-		System.out.println("欧米 : " + factory.get("omi").trans(LANG.EN, LANG.ZH, origin));
+		System.out.println("金山 : " + factory.get("jinshan").trans(LANG.EN, LANG.TR, origin));
+		System.out.println("有道 : " + factory.get("youdao").trans(LANG.EN, LANG.TR, origin));
+		System.out.println("百度 : " + factory.get("baidu").trans(LANG.EN, LANG.TR, origin));
+		System.out.println("谷歌 : " + factory.get("google").trans(LANG.EN, LANG.TR, origin));
+		System.out.println("腾讯 : " + factory.get("tencent").trans(LANG.EN, LANG.TR, origin));
+		System.out.println("欧米 : " + factory.get("omi").trans(LANG.EN, LANG.TR, origin));
 		System.out.println("\n");
 		
 	
 		origin = "这个巡展，大家想去的先注册，临近当地巡展时候，会有注册确认邮件发给大家，届时可以看到详细的活动地址。\n等你工作了你就知道你当时的观念是多么的幼稚";
-		System.out.println("金山 : " + factory.get("jinshan").trans(LANG.ZH, LANG.EN, origin));
-		System.out.println("有道 : " + factory.get("youdao").trans(LANG.ZH, LANG.EN, origin));
-		System.out.println("百度 : " + factory.get("baidu").trans(LANG.ZH, LANG.EN, origin));
-		System.out.println("谷歌 : " + factory.get("google").trans(LANG.ZH, LANG.EN, origin));
-		System.out.println("腾讯 : " + factory.get("tencent").trans(LANG.ZH, LANG.EN, origin));
-		System.out.println("欧米 : " + factory.get("omi").trans(LANG.ZH, LANG.EN, origin));
+		System.out.println("金山 : " + factory.get("jinshan").trans(LANG.TR, LANG.EN, origin));
+		System.out.println("有道 : " + factory.get("youdao").trans(LANG.TR, LANG.EN, origin));
+		System.out.println("百度 : " + factory.get("baidu").trans(LANG.TR, LANG.EN, origin));
+		System.out.println("谷歌 : " + factory.get("google").trans(LANG.TR, LANG.EN, origin));
+		System.out.println("腾讯 : " + factory.get("tencent").trans(LANG.Tr, LANG.EN, origin));
+		System.out.println("欧米 : " + factory.get("omi").trans(LANG.TR, LANG.EN, origin));
 	}
 	
 	/*
@@ -45,7 +45,7 @@ public class TransTest {
 		
 		while(true){
 			cnt++;
-			System.out.println(cnt+" : "+lator.trans(LANG.EN, LANG.ZH, origin));
+			System.out.println(cnt+" : "+lator.trans(LANG.EN, LANG.TR, origin));
 			Thread.sleep(10);
 		}
 	}
